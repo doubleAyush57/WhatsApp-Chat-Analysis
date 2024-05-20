78c138061e74fc47bc241b277e5bf85441a5805c
Aayush <aayush.talreja@gmail.com>
2024-05-19 21:32:57 -0400

    test

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import pandas as pd
+import threading as th
+import time as t
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# String Constants
+WELCOME_MESSAGE = "Welcome to WhatsApp Chat Processor"
+LINE_BREAK = "--------------"
+FILE_NAME = "input.txt"
 
+# Welcome and Reading File
+print(WELCOME_MESSAGE)
+print(LINE_BREAK)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-test
+f = open(FILE_NAME, "r", encoding="utf-8")
+lines = f.readlines()
+f.close()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+data = []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for line in lines:
+    # Processing Each Line
+    date_split_index = line.find(", ")
+    date = line[:date_split_index]
+
+    time_split_index = line[date_split_index:].find("M")
+    time = line[date_split_index + len(", "): date_split_index + time_split_index - 1]
+
+    am_pm = line[date_split_index + time_split_index - 1: date_split_index + time_split_index + 1]
+
+    sender_split_index = line.find(' - ')
+    sender = line[sender_split_index + len(' - '):line.find(": ")]
+
+    message = line[line.find(": ") + 2:]
+
+    data.append({"date" : date,
+                 "time" : time,
+                 "am_pm" : am_pm,
+                 ""
+                 })
