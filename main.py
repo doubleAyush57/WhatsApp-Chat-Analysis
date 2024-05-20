4ef1684726cfae312046f291b728267d0dc24f25
Aayush <aayush.talreja@gmail.com>
2024-05-20 00:07:01 -0400

    test

--- main.py
+++ main.py
@@ -2,38 +2,51 @@ import pandas as pd
 import time as t
 
 # String Constants
-WELCOME_MESSAGE = "Welcome to WhatsApp Chat Processor"
-LINE_BREAK = "--------------"
 FILE_NAME = "input.txt"
 
 # Welcome and Reading File
-print(LINE_BREAK)
-print(WELCOME_MESSAGE)
+print("--------------")
+print("Welcome to WhatsApp Chat Processor")
 
 f = open(FILE_NAME, "r", encoding="utf-8")
 lines = f.readlines()
 f.close()
 
 data = []
+senders = []
+words = {}
 start_time = t.time()
+instagram_count = 0 
 
 for line in lines:
-    # Processing Each Line
     date_split_index = line.find(", ")
+    if date_split_index == -1:
+        continue
     date = line[:date_split_index]
 
     time_split_index = line[date_split_index:].find("M")
-    time = line[date_split_index + len(", "): date_split_index + time_split_index - 1]
+    if time_split_index == - 1 or line.find("M") == - 1:
+        continue
+    time = line[date_split_index + len(", "): date_split_index + time_split_index - 2]
 
     am_pm = line[date_split_index + time_split_index - 1: date_split_index + time_split_index + 1]
 
     sender_split_index = line.find(' - ')
+    if line.find(" - ") == - 1:
+        continue
     sender = line[sender_split_index + len(' - '):line.find(": ")]
+    if sender not in senders:
+        senders.append(sender)
 
     message = line[line.find(": ") + 2:]
-
-    if date_split_index == - 1 or time_split_index == - 1 or sender_split_index == - 1 or line.find(": ") == - 1 or line.find("M") == - 1 or line.find(' - ') == - 1:
-        continue
+    word_list = message.split(" ")
+    for word in word_list:
+        if word in words and "<" not in word and ">" not in word:
+            words[word] += 1
+            if "www.instagram" in word:
+                instagram_count += 1
+        else:
+            words[word.strip()] = 1
 
     data.append({"date": date,
                  "time": time,
@@ -42,13 +55,28 @@ for line in lines:
                  "message": message
                  })
 
-
 myDF = pd.DataFrame(data)
 end_time = t.time()
-print("File Processing Completed in", round((end_time - start_time) * 1000, 3), "milliseconds")
 
-senders = []
-for user in myDF["sender"]:
-    if user not in senders:
-        senders.append(user)
+if len(senders) == 2:
+    print("DM Between", senders[0], "and", senders[1])
+else:
+    print("GC Among:", ", ".join(senders))
 
+print("--------------")
+
+print(FILE_NAME, "| File Processing Completed in", round((end_time - start_time) * 1000, 3), "milliseconds")
+print("Handled", len(myDF), "messages |", myDF["date"][0], "at", myDF["time"][0], myDF["am_pm"][0], "to",
+      myDF["date"][len(myDF) - 1], "at", myDF["time"][len(myDF) - 1], myDF["am_pm"][len(myDF) - 1])
+
+sorted_words = sorted(words.items(), key=lambda x: x[1], reverse=True)
+
+count = 1
+w = open("output.txt", "w", encoding="utf-8")
+for i in range(len(sorted_words) - 1):
+    w.write(str(count) + " | uses: " + str(sorted_words[i][1]) + " | " + sorted_words[i][0] + "\n")
+    count += 1
+
+w.close()
+
+print("Instagram Count:", instagram_count)
